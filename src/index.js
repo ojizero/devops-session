559a163d08326c2369d400d26bc74169ddc0dfa7
ojizero <z.acnts.816@protonmail.ch>
2018-03-26 10:32:20 +0200

    sls with webpack, travis configs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-'use strict';
+import { DynamoDB } from 'aws-sdk'
 
-module.exports.hello = (event, context, callback) => {
+export const increment = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+export const count = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
