bee36871c883e669e5effecc0ab2ab37f1c9f9d5
ojizero <z.acnts.816@protonmail.ch>
2018-03-26 11:58:14 +0200

    restructure

---
+++ src/increment.js
@@ -0,0 +1,50 @@
+import { DynamoDB } from 'aws-sdk'
+
+export default (event, context, callback) => {
+  const queryParams = event.pathParameters || {}
+
+  const yourCookie = queryParams.my_cookie || 'no_cookie'
+
+  const dynamo = new DynamoDB()
+
+  return Promise.resolve()
+    .then(() => {
+      return dynamo
+        .getItem({
+          Key: {
+            cid: {
+              S: yourCookie
+            }
+          },
+          TableName: process.env.DYNAMO_TABLE_NAME,
+        })
+        .promise()
+    })
+    .then(({ Item: item }) => {
+      const currentCount = Number(item.count.N)
+
+      return currentCount + 1
+    })
+    .catch(() => 0) // errors mean that the item doesn't exist
+    .then((newCount = 0) => {
+      return dynamo
+        .putItem({
+          Item: {
+            cid: {
+              S: yourCookie
+            },
+            count: {
+              N: JSON.stringify(newCount) // because Amazon :rolling_eyes:
+            },
+          },
+          TableName: process.env.DYNAMO_TABLE_NAME,
+        })
+        .promise()
+    })
+    .then(resp => {
+      return callback(null, { statusCode: 200, body: JSON.stringify(resp) })
+    })
+    .catch(err => {
+      return callback(null, { statusCode: 500, body: JSON.stringify(err) })
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,72 +1,9 @@
-import { DynamoDB } from 'aws-sdk'
+import increment from './increment'
+import countAll from './count-all'
+import countEach from './count-each'
 
-export const increment = (event, context, callback) => {
-  const queryParams = event.pathParameters || {}
-
-  const yourCookie = queryParams.my_cookie || 'no_cookie'
-
-  const dynamo = new DynamoDB()
-
-  return Promise.resolve()
-    .then(() => {
-      return dynamo
-        .getItem({
-          Key: {
-            cid: {
-              S: yourCookie
-            }
-          },
-          TableName: process.env.DYNAMO_TABLE_NAME,
-        })
-        .promise()
-    })
-    .then(({ Item: item }) => {
-      const currentCount = Number(item.count.N)
-
-      return currentCount + 1
-    })
-    .catch(() => 0) // errors mean that the item doesn't exist
-    .then((newCount = 0) => {
-      return dynamo
-        .putItem({
-          Item: {
-            cid: {
-              S: yourCookie
-            },
-            count: {
-              N: JSON.stringify(newCount) // because Amazon :rolling_eyes:
-            },
-          },
-          TableName: process.env.DYNAMO_TABLE_NAME,
-        })
-        .promise()
-    })
-    .then(resp => {
-      return callback(null, { statusCode: 200, body: JSON.stringify(resp) })
-    })
-    .catch(err => {
-      return callback(null, { statusCode: 500, body: JSON.stringify(err) })
-    })
-}
-
-export const countAll = (event, context, callback) => {
-  const dynamo = new DynamoDB()
-
-  return Promise.resolve()
-    .then(() => {
-      return dynamo
-        .scan({
-          TableName: process.env.DYNAMO_TABLE_NAME,
-        })
-        .promise()
-    })
-    .then(({ Items: items }) => {
-      return items.reduce((acc, item) => acc + Number(item.count.N), 0)
-    })
-    .then(total => {
-      return callback(null, { statusCode: 200, body: JSON.stringify({ total }) })
-    })
-    .catch(err => {
-      return callback(null, { statusCode: 500, body: JSON.stringify({ err }) })
-    })
+export {
+  increment,
+  countAll,
+  countEach,
 }
