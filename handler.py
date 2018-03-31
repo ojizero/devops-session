5abe097569a364285e809bdcde4ae2965d6d972e
ojizero <z.acnts.816@protonmail.ch>
2018-03-31 13:57:50 +0300

    increment function

--- handler.py
+++ handler.py
@@ -4,17 +4,44 @@ import boto3
 
 from random import randint
 
-def insert_to_dynamo(event, context):
+table_name = os.environ['DYNAMO_TABLE']
+
+def increment (event, context):
     client = boto3.client('dynamodb')
 
+    visitor_id = event \
+        .get('queryStringParameters', {}) \
+        .get('visitor_id', None)
+
+    if visitor_id is None:
+        visitor_id = event['requestContext']['identity']['sourceIp']
+
+    old_visits = 0
+    try:
+        response = client.get_item(
+            TableName = table_name,
+            Key = {
+                'visitor_id': {
+                    'S': visitor_id,
+                },
+            }
+            AttributesToGet = [
+                'visits'
+            ]
+        )
+
+        old_visits = response['Item']['visits']['N']
+    except:
+        pass
+
     response = client.put_item(
-        TableName = os.environ['DYNAMO_TABLE'],
+        TableName = table_name,
         Item = {
             'visitor_id': {
                 'S': 'some static id for now',
             },
             'visits': {
-                'N': str(randint(10, 1000)),
+                'N': str(old_visits + 1),
             },
         }
     )
